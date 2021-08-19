77430e16a9f10abce792a3e7a0299770d333208a
unknown <deepu.03feb@gmail.com>
2021-08-19 17:16:17 +0530

    service changes

--- src/app/http-serve.service.ts
+++ src/app/http-serve.service.ts
@@ -36,7 +36,7 @@ export class HttpServeService {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
+        `Backend returned code ${error.status}, body is: `, error.error);
     }
     // Return an observable with a user-facing error message.
     return throwError(
